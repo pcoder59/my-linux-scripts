64b9e5c8231eaeab77f98b8edac251c1da60f6bb
Prem Raj <53263555+pcoder59@users.noreply.github.com>
2024-09-05 20:34:13 +0530

    bash zsh vi mode

--- copy-configs.sh
+++ copy-configs.sh
@@ -2,3 +2,6 @@ CONFIG="$HOME/.config"
 DOTFILES="$HOME/Documents/Dotfiles"
 cp "$CONFIG/kak/kakrc" "$DOTFILES/.config/kak/"
 cp -r "$CONFIG/vifm" "$DOTFILES/.config/"
+cp "$HOME/.zshrc" "$DOTFILES"
+cp "$HOME/.bashrc" "$DOTFILES"
+
